5ce2985d2cd6211c8e0bc42530f5e596a81eccd8
rashmpad <37831236+rashmpad@users.noreply.github.com>
2018-04-02 15:35:30 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -27,7 +27,7 @@ fi
 # --- Configs:
 
 echo " (i) Provided app path: ${app_path}"
-echo " (i) Provided app center app: ${app_center_app}"
+echo " (i) Provided app center app: rashmi.padhi-01/Petco-QA"
 echo " (i) Provided app center token: 4cf23d2f1d9c268284ace5a1cc8bd374f4271b64"
 echo
 
