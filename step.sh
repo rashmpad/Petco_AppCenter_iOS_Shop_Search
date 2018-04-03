366b179f2ad04304829beec0b46baf9d5b5754bd
rashmpad <37831236+rashmpad@users.noreply.github.com>
2018-04-03 15:54:59 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -47,7 +47,7 @@ nuget restore -NonInteractive "${SOLUTION}"
 msbuild "${SOLUTION}" /p:Configuration=Release
 
 appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --debug --quiet
-appcenter test run uitest --app "rashmi.padhi-01/Petco_QA_iOS" --devices "rashmi.padhi-01/apple-iphone-7-plus" --app-path /Users/rashmi.padhi/Documents/GitHubiOS/bitrise-step-app-center-launch-test-iOS/_tmp/Petco.iOS.ipa  --fixture Petco.UITests.Cart\(iOS\).VerifyCartFlowSecureCheckOutForgotPasswordwithOutRepeatDelivery --test-series "master" --locale "en_US" --build-dir "/Users/rashmi.padhi/Documents/Srini_AutomationBranch/Petco.UITests/bin/Debug"
+appcenter test run uitest --app "${app_center_app}" --devices "rashmi.padhi-01/apple-iphone-7-plus" --app-path "${app_path}" --async --fixture Petco.UITests.Cart\(iOS\).VerifyCartFlowSecureCheckOutForgotPasswordwithOutRepeatDelivery --test-series "master" --locale "en_US" --token "${app_center_token}" --build-dir "/Users/rashmi.padhi/Documents/Srini_AutomationBranch/Petco.UITests/bin/Debug"
 #
 # --- Exit codes:
 # The exit code of your Step is very important. If you return
